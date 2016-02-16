d3daad745b530b44cc8003aad21ad2f3295c5ca1
xete <xete@github.com>
2016-02-16 18:19:32 +0800

    smartconf layout

--- src/com/xete/esptiny/EspTinyActivity.java
+++ src/com/xete/esptiny/EspTinyActivity.java
@@ -10,6 +10,6 @@ public class EspTinyActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.smartconfig);
     }
 }
