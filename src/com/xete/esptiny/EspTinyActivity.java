a150977fae7a3f75d9c4cfdbc9a3375918c815df
xete <xete@github.com>
2016-02-17 17:05:39 +0800

    merget EsptouchActivity

--- src/com/xete/esptiny/EspTinyActivity.java
+++ src/com/xete/esptiny/EspTinyActivity.java
@@ -1,15 +1,37 @@
 package com.xete.esptiny;
 
+import com.espressif.iot.esptouch.esptouch_activity.EsptouchActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
-public class EspTinyActivity extends Activity
+public class EspTinyActivity extends Activity implements OnClickListener
 {
+	
+	private Button mButtonLogin;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.smartconfig);
+        setContentView(R.layout.main);
+		
+		mButtonLogin = (Button) findViewById(R.id.buttonLogin);
+		mButtonLogin.setOnClickListener(this);
     }
+
+		
+	@Override
+	public void onClick(View v) {
+		if(v == mButtonLogin) {
+			Intent intent = new Intent();
+			intent.setClass(EspTinyActivity.this, EsptouchActivity.class);
+			EspTinyActivity.this.startActivity(intent);
+			EspTinyActivity.this.finish();
+		}
+	}
 }
