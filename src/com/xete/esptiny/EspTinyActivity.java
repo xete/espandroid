ab054284a015d257510bdab13736b12645937745
xete <xete@github.com>
2016-02-17 22:57:41 +0800

    add ControlActivity

--- src/com/xete/esptiny/EspTinyActivity.java
+++ src/com/xete/esptiny/EspTinyActivity.java
@@ -4,6 +4,8 @@ import com.espressif.iot.esptouch.esptouch_activity.EsptouchActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
@@ -12,6 +14,7 @@ public class EspTinyActivity extends Activity implements OnClickListener
 {
 	
 	private Button mButtonLogin;
+	private Button mButtonLogout;
 
     /** Called when the activity is first created. */
     @Override
@@ -21,15 +24,22 @@ public class EspTinyActivity extends Activity implements OnClickListener
         setContentView(R.layout.main);
 		
 		mButtonLogin = (Button) findViewById(R.id.buttonLogin);
+		mButtonLogout = (Button) findViewById(R.id.buttonLogout);
 		mButtonLogin.setOnClickListener(this);
+		mButtonLogout.setOnClickListener(this);
+
     }
 
 		
 	@Override
 	public void onClick(View v) {
-		if(v == mButtonLogin) {
+		if(v != null) {
 			Intent intent = new Intent();
-			intent.setClass(EspTinyActivity.this, EsptouchActivity.class);
+			if(v == mButtonLogin) {
+				intent.setClass(EspTinyActivity.this, EsptouchActivity.class);
+			}else if(v == mButtonLogout) {
+				intent.setClass(EspTinyActivity.this, ControlActivity.class);	
+			}
 			EspTinyActivity.this.startActivity(intent);
 			EspTinyActivity.this.finish();
 		}
